35d9dd871438ea6b995b2a008255c2d8fe99b948
Anderson Ventosilla <andersonventosilla0@gmai.com>
2022-04-07 21:03:02 -0500

    terminado

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -4,15 +4,21 @@ import '../css/Testimonio.css'
 function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
-      <img 
-        className='imagen-testimonio' 
-        src={require('../imagenes/testimonio-emma.png')}
-        alt='Foto de emma'
+      <img
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt={`Foto de ${props.imagen}`} 
       />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
-        <p className='texto-testimonio'>{props.testimonio}</p>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en <strong>{props.pais}</strong>
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'>
+          "{props.testimonio}"
+        </p>
       </div>
     </div>
   )
